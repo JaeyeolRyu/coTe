66e2b8bce28364747f8499af076ce1c42f5a8fb1
JaeyeolRyu <win100477@naver.com>
2021-05-03 20:22:51 +0900

    5/3 cordingTest

---
+++ src/coTe/KakaoCompression.java
@@ -0,0 +1,66 @@
+package coTe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class KakaoCompression {
+	
+	public static void main(String[] args) {
+
+		String msg = "KAKAO";
+		
+		int[] answer = solution(msg);
+		
+		System.out.println(Arrays.toString(answer));
+        
+	}
+    public static int[] solution(String msg) {
+        int[] answer = {};
+        List<Integer> answerList = new ArrayList<Integer>();
+        
+        Map<String, Integer> dicMap = new HashMap<String, Integer>();
+        StringBuilder sb = new StringBuilder();
+        String tempStr = "";
+        int dicIdx = 1;
+        for(char c = 'A'; c <= 'Z'; c++ ) {
+        	
+        	dicMap.put(String.valueOf(c), dicIdx);
+        	dicIdx++;
+        }
+        
+        for(int i = 0 ; i < msg.length(); i++ ) {
+        	
+        	sb.append(msg.charAt(i));
+        	
+        	if(dicMap.containsKey(sb.toString())) {
+        		tempStr = sb.toString();
+        		
+        		if(i == msg.length()-1) {
+        			answerList.add(dicMap.get(tempStr));
+        			break;
+        		}
+        		continue;
+        	} 
+        	// 여기 온거면 키가 없다는뜻
+        	
+        	answerList.add(dicMap.get(tempStr)); // 직전에 dicMap에 존재했던 키의 value값을 answerList 넣음.
+        	
+        	dicMap.put(sb.toString(), dicIdx++); // 다음글자까지 입력해놓은 stringbuilder를 map에 색인과 함께 저장함.
+        	i--; 		//다음글자를 현재글자로 시작하기 위함.
+        	sb = new StringBuilder();
+        	
+        }
+ 
+        answer = new int[answerList.size()];
+        
+        for(int i = 0; i < answer.length; i++) {
+        	
+        	answer[i] = answerList.get(i);
+        }
+        
+        return answer;
+    }
+}
--- src/coTe/Solution.java
+++ src/coTe/Solution.java
@@ -1,105 +1,163 @@
 package coTe;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 class Solution {
-	static Map<String, String> songInfo = new HashMap<>();
-    static Map<Integer, String> posSong = new HashMap<>();
-    
+	
+	static ArrayList<Map<Integer, String>> sortList = new ArrayList<>();
+	
 	public static void main(String[] args) {
 		
-		String m = "CC#BCC#BCC#BCC#B";
-		String[] musicinfos = {"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"};
+		String[] files = {"foo9.txt", "foo010bar020.zip","F-15","a-00000"};
+		String[] ans = solution(files);
 		
-		String answer = solution(m, musicinfos);
-		
-		System.out.println(answer);
+		System.out.println(Arrays.toString(ans));
 	}
-
-	public static String solution(String m, String[] musicinfos) {
-        String answer = "";
-        String melody = m.replace("A#", "a").replace("B#", "b").replace("C#", "c").replace("D#", "d").replace("E#", "e").replace("F#", "f").replace("G#", "g"); //#처리를 해주기위해 #앞에 문자를 소문자로 바꾼다.
-        System.out.println("들은노래 " + melody);
-        int songTime = 0;
-        String songName ="";	
-        String songMelody ="";
+	
+	
+    public static String[] solution(String[] files) {
+        String[] answer = {};
         
-        for(String str : musicinfos) {
+        for(String str : files) {
         	
-        	String[] tempArr = str.split(":|,");
-        	songTime = (Integer.parseInt(tempArr[2])*60+Integer.parseInt(tempArr[3])) - (Integer.parseInt(tempArr[0])*60+Integer.parseInt(tempArr[1]));
-
-        	songName = tempArr[4];
-        	songMelody = tempArr[5].replace("A#", "a").replace("B#", "b").replace("C#", "c").replace("D#", "d").replace("E#", "e").replace("F#", "f").replace("G#", "g"); //#처리를 해주기위해 #앞에 문자를 소문자로 바꾼다.
-
-        	String totalMelody = "";
-        	int tempNum = 0;
-        	System.out.println("노래길이 " +songMelody);
-        	for(int i = 0; i< songTime; i++ ) {
+        	String head = "";
+        	String body = "";
+        	String tail = "";
+        	int idx = 0;
+        	for(idx = 0 ; idx < str.length(); idx++) { //head찾기
         		
-    			totalMelody += songMelody.charAt(tempNum);
-        	
-        		tempNum++;
-        		if(tempNum == songMelody.length()) {
-        			tempNum = 0;
+        		char tempChar = str.charAt(idx);
+        		
+        		if((tempChar >= 'a' && tempChar <= 'z') || (tempChar >= 'A' && tempChar <='Z') || tempChar == '-' || tempChar == '.' || tempChar == ' ') {
+        			head += tempChar;
+        		} else {
+        			break;
         		}
         	}
-        	System.out.println("토탈길이 " +totalMelody);
-        	songInfo.put(songName, totalMelody);
-        }
-        
-        Iterator<Map.Entry<String, String>> it = songInfo.entrySet().iterator();
-        
-        while(it.hasNext()) { // 일치하는 곳 있는지 확인
-        	
-        	Map.Entry<String, String> entry = it.next();
-        	String sName = entry.getKey();
-        	String tMelody = entry.getValue();
-    
-        	if(tMelody.length() <= melody.length()) { 
-        		
-        		if(melody.substring(0, tMelody.length()).equals(tMelody)) {
-        			
-        			posSong.put(tMelody.length(), sName);
+        	int i = 0;
+        	for( i = idx; i < str.length(); i++ ) { // body찾기
+        		char tempChar = str.charAt(i);
+        		if(tempChar >= '0' && tempChar <= '9') {
+        			body += tempChar;
+        			if(body.length() == 5) {
+        				i++;
+        				break;
+        			}
+        		} else {
+        			break;
         		}
-        			
-        	} else {
-        		
-            	for(int i = 0 ; i <= tMelody.length() - melody.length(); i++) {
-            		if(melody.charAt(0) == tMelody.charAt(i)) {
-            			if(melody.equals(tMelody.substring(i, i+melody.length()))) {
-            				
-            				if(!posSong.containsKey((Object)sName)) { // 시간이 같은 곡이있다면 먼저 들어온곡으로 하기위함
-            					posSong.put(tMelody.length(), sName);
-            					
-            				}
-            				break;
-            			}
-            		}
-            	}	
-            }
+        	}
+        	
+    		tail = str.substring(i);
+    		
+//        	System.out.println("헤드: " + head + " 바디: " + body + " 꼬리: " +tail);
+        	sortingHead(head, body, tail);
+        	
         }
+        
+        answer = new String[sortList.size()];
 
-        if(posSong.isEmpty()) {
-        	return "(None)";
-        } else {
+
+        for(int i = 0 ; i < answer.length ; i++) {
         	
-//        	TreeMap<Integer, String> treeMap = new TreeMap<>(Collections.reverseOrder());
-//        	treeMap.putAll(posSong);
-//        	
-//        	answer = treeMap.get(treeMap.firstKey());
-        	
-        	Object[] keySet = posSong.keySet().toArray();
-        	Arrays.sort(keySet, Collections.reverseOrder());
-        	answer = posSong.get(keySet[0]);
+        	answer[i] = sortList.get(i).get(1)+sortList.get(i).get(2)+sortList.get(i).get(3);       	
         }
         
         return answer;
     }
     
+    public static void sortingHead(String head, String body, String tail) {
+    	Map<Integer,String> nameMap = new HashMap<Integer, String>();
+    	if(sortList.isEmpty()) {
+    		nameMap.put(1, head);
+    		nameMap.put(2, body);
+    		nameMap.put(3, tail);
+    		sortList.add(nameMap);
+    		return;
+    		
+    	} else {
+    		for(int i = 0; i < sortList.size(); i++ ) {
+    			int headLength = 0;
+    			boolean isLong = false; // 인덱스값의 헤드가 길경우 true, 아닐시 false 
+    			String inputHead = head.toUpperCase();
+    			String compareHead = sortList.get(i).get(1).toUpperCase();
+    			
+    			if(inputHead.equals(compareHead)) {
+					sortingBody(head,body,tail,i);
+					return;
+    			}
+    			
+    			if(inputHead.length() < compareHead.length()) { 
+    				headLength = inputHead.length();
+    				isLong = true;
+    			} else {
+    				headLength = compareHead.length();
+    				
+    			}
+
+				for(int j = 0; j < headLength; j++) {
+					
+					if(inputHead.charAt(j) == compareHead.charAt(j)) {
+						continue; // 같으면 다음문자열 확인위해 continue;
+						
+					} else if(inputHead.charAt(j) > compareHead.charAt(j) ) {
+						break; // 들어온값의 헤드가 크므로 다음인덱스의 헤드와 비교하기위해 break;
+						
+					} else { //기존인덱스에 있던 값이 큼.
+			    		nameMap.put(1, head);
+			    		nameMap.put(2, body);
+			    		nameMap.put(3, tail);
+			    		sortList.add(i,nameMap);
+			    		return;
+					}
+					
+				}
+    			
+				if(isLong) {
+					nameMap.put(1, head);
+		    		nameMap.put(2, body);
+		    		nameMap.put(3, tail);
+		    		sortList.add(i,nameMap);
+		    		return;
+				}
+				
+    		} // 처리안됌 즉 기존 인덱스값들 다확인한 것보다 크다는 뜻이니 맨뒤에다가 삽입
+    		
+    		nameMap.put(1, head);
+    		nameMap.put(2, body);
+    		nameMap.put(3, tail);
+    		sortList.add(nameMap);
+    		return;
+    		
+    	}
+
+    }
     
+    public static void sortingBody(String head, String body, String tail, int idx) { //헤드부분이 같으면 옴.
+    	Map<Integer,String> nameMap = new HashMap<Integer, String>();
+    	for(int i = idx ; i < sortList.size(); i++) {
+    		 
+    		int inputBody = Integer.parseInt(body);
+    		int compareBody = Integer.parseInt(sortList.get(i).get(2));
+
+    		if(inputBody < compareBody) {
+    			
+        		nameMap.put(1, head);
+        		nameMap.put(2, body);
+        		nameMap.put(3, tail);
+        		sortList.add(i,nameMap);
+    			return;
+    		}
+    	}
+    								//마지막까지 다돌았는데 값이더 크므로 맨 뒤에다가 삽입.
+		nameMap.put(1, head);
+		nameMap.put(2, body);
+		nameMap.put(3, tail);
+		sortList.add(nameMap);
+		return;
+    }
+
 }
