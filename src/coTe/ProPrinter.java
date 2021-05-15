68dfac1c1bf96e3dcc0302765bcac5f846c5b2bf
JaeyeolRyu <win100477@naver.com>
2021-05-15 18:05:45 +0900

    5/15 cordingTest

---
+++ src/coTe/ProPrinter.java
@@ -0,0 +1,66 @@
+package coTe;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class ProPrinter {
+
+	public static void main(String[] args) {
+
+		int[] priorities = { 1, 1, 9, 1, 1, 1 };
+		int location = 0;
+
+		int answer = solution(priorities, location);
+
+		System.out.println(answer);
+
+	}
+
+	public static int solution(int[] priorities, int location) {
+		int answer = 1;
+		int idx = 0;
+		Integer[] sortArr = Arrays.stream(priorities.clone()).boxed().toArray(Integer[]::new);
+
+		Arrays.sort(sortArr, Comparator.reverseOrder());
+		Queue<Integer> q = new LinkedList<Integer>();
+
+		for (int num : priorities) {
+
+			q.add(num);
+
+		}
+
+		while (!q.isEmpty()) {
+
+			if (q.peek() == sortArr[idx]) {
+
+				if (location == 0) {
+					return answer;
+				} else {
+
+					location--;
+					q.poll();
+					answer++;
+					idx++;
+				}
+
+			} else {
+
+				q.add(q.poll());
+
+				if (location == 0) {
+
+					location = q.size() - 1;
+
+				} else {
+					location--;
+				}
+			}
+
+		}
+		return answer;
+	}
+
+}
--- src/coTe/Solution.java
+++ src/coTe/Solution.java
@@ -1,100 +1,38 @@
 package coTe;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
-	static int totalDis = 0;
-	static int skipIdx = 0;
-	static int changeLetter = 0;
-	static String name = "";
-	
-	public static void main(String[] args) {
-//		String name = "JEROEN";
-		String name = "JAN";
-//		String name = "ABABAAAAABA";
+    public String solution(int[] numbers) {
+        String[] strArr = new String[numbers.length];
+        
+        for(int i = 0 ; i < numbers.length; i++) {
+        	strArr[i] = String.valueOf(numbers[i]);
+        }
 
-		int answer = solution(name);
-		System.out.println(totalDis);
-		System.out.println(answer);
+        Arrays.sort(strArr, new Comparator<String>() {
 
-	}
-
-	public static int solution(String names) {
-		int answer = 0;
-		name = names;
-		skipIdx = name.length();
-		int[] alphabet = new int[26];
-
-		for (int i = 0; i < 26; i++) { // A가 0, B가 1 ...
-			alphabet[i] = Math.min(i, Math.abs(i - 26));
-		}
-
-		if (name.length() == 1) {
-			
-			return alphabet[name.charAt(0) - 'A'];
-
-		} else { // 앞 뒤중에 어느쪽으로 가는게 이득인지 따져봐야함. 움직이는 거리와 알파벳을 따로계산
-			
-			for(int i = 0 ; i < name.length(); i++) { //알파벳 바꾸는데 필요한 횟수
-				
-				changeLetter += alphabet[name.charAt(i) - 'A'];
+			@Override
+			public int compare(String o1, String o2) {
+				return (o2+o1).compareTo(o1+o2);
 			}
-			
-		}
-		
-		fowardDir(1);
-		
-		answer = totalDis + changeLetter;
-		
-		return answer;
-	}
-	
-	public static void fowardDir(int idx) {
-		
-		int dis = 0; //되돌아갈때 필요한 추가거리
-		for (int i = idx; i < name.length(); i++) { // 정방향 이동 중 연속된 A갯수
-			int cnt = 0;
+		});
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0 ; i < strArr.length; i++)	 {
+        	
+        	sb.append(strArr[i]);
+        	
+        }
+        
+        if(sb.toString().charAt(0) == '0') {
+        	return "0";
+        } else {
+        	return sb.toString();
+        }
 
-			if (name.charAt(i) == 'A') {
-				
-				for( int j = i; j < name.length(); j++) {
-					
-					if (name.charAt(j) == 'A') {
-						cnt++;
-					} else {
-						break;
-					}
-				}
-				
-				if(dis < cnt+1) { 
-					
-					skipIdx = i+cnt;
-					totalDis += dis+1;
-					reverseDir(name.length()-1);
-					return;
-				} else { // A를 지나가는게 낫다면 A부분을 건너뛴 거리를 구해서 i에 대입해줘야 함.
-					
-					totalDis += cnt+1;
-					dis += cnt+1;
-					i += cnt;
-					
-				}
-				
-			} else {
-				totalDis++;
-				dis++;
-			}
-
-		}
-		
-	}
-	
-	public static void reverseDir(int idx) {
-		
-		for (int i = idx; i > skipIdx; i--) { // 정방향 이동 중 연속된 A갯수
-							
-			totalDis++;
-
-		}
-		
-	}
-	
+   
+    }
 }
