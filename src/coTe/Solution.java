e656ade79dfb8a33548c1042964d0ea57c079c0a
JaeyeolRyu <win100477@naver.com>
2021-05-18 18:20:02 +0900

    5/18 cordingTest

--- src/coTe/Solution.java
+++ src/coTe/Solution.java
@@ -1,63 +1,38 @@
 package coTe;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 class Solution {
-
+	
 	public static void main(String[] args) {
-
-		int bridge_length = 100;
-		int weight = 100;
-		int[] truck_weights = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
-
-		int ans = solution(bridge_length, weight, truck_weights);
-		System.out.println(ans);
+		
+		long[] numbers = {2,7};
+		long[] ans = solution(numbers);
+		System.out.println(Arrays.toString(ans));
+		
 	}
-
-	public static int solution(int bridge_length, int weight, int[] truck_weights) {
-		int answer = 0;
-		int loadWeight = 0;
-		Queue<Integer> q = new LinkedList<Integer>();
-
-		for (int i = 0; i < truck_weights.length; i++) {
-
-			while (true) {
-
-				if (q.isEmpty()) {
-
-					q.add(truck_weights[i]);
-					loadWeight += truck_weights[i];
-					answer++;
-					break;
-
-				} else if (q.size() == bridge_length) {
-
-					loadWeight -= q.poll();
-
-				} else {
-
-					if (truck_weights[i] + loadWeight > weight) {
-
-						q.offer(0);
-						answer++;
-
-					} else {
-
-						q.offer(truck_weights[i]);
-						answer++;
-						loadWeight += truck_weights[i];
-						break;
-					}
-
-				}
-
-			}
-
-		}
-
-		answer += bridge_length;
-
-		return answer;
-	}
+    public static long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        
+        int idx = 0;
+        for(long temp : numbers) {
+        	
+    		long num = temp;
+    		int pow = 0;
+    		while(num >= 0) {
+    			
+    			if(num%2 == 0) {
+    				answer[idx] = temp+(long)Math.pow(2, pow)-(long)Math.pow(2, pow-1);
+    				break;
+    			}
+    			pow++;
+    			num /= 2;
+    			
+    		}
+    		
+    		idx++;
+        }
+    		
+        return answer;
+    }
 }
