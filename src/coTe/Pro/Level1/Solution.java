73af2ce4e1c4e605d5ee4ac65d793cd4830ad90b
JaeyeolRyu <win100477@naver.com>
2021-06-26 14:15:50 +0900

    6/26 cordingTest

--- src/coTe/Pro/Level1/Solution.java
+++ src/coTe/Pro/Level1/Solution.java
@@ -1,21 +1,5 @@
 package coTe.Pro.Level1;
 
-class Solution {
-	
-    public boolean solution(String s) {
-        boolean answer = true;
-        
-        if(s.length() != 4 && s.length() != 6) {
-        	return false;
-        }
-        
-        for(char c : s.toCharArray()) {
-        	if(c >= '0' && c <= '9') {
-        		continue;
-        	}
-        	return false;
-        }
-        
-        return answer;
-    }
+public class Solution {
+
 }
