a558d7264782ab223e9ff8081aaf47d5fe4245f4
JaeyeolRyu <win100477@naver.com>
2021-06-27 21:16:17 +0900

    6/27 cordingTest

--- src/coTe/Pro/Level1/Solution.java
+++ src/coTe/Pro/Level1/Solution.java
@@ -1,5 +1,32 @@
 package coTe.Pro.Level1;
 
-public class Solution {
+class Solution {
+	public int solution(int n) {
+		int[] dp = new int[n + 1];
 
+		for (int i = 2; i <= n; i++) {
+
+			if (calc(i)) {
+				dp[i] = dp[i - 1] + 1;
+			} else {
+				dp[i] = dp[i - 1];
+			}
+
+		}
+
+		return dp[n];
+	}
+
+	public boolean calc(int num) {
+
+		for (int i = 2; i < num; i++) {
+
+			if (num % i == 0) {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
 }
